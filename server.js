09ad86e921e131895b9a2f6332fbb6fa045fe5dc
Manjil_Itani <manjilitani3@gmail.com>
2022-09-21 18:02:06 -0400

    Initilized necessary routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send({ msg: 'Contact Keeper Coming soon' }));
+
+//Define routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server for Contact-Keeper2 started on port ${PORT}`)
+);
