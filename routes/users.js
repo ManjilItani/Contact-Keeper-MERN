1a73aee3258b86d2883132a565fd4d6001aa18a3
Manjil_Itani <manjilitani3@gmail.com>
2022-09-30 09:46:56 -0400

    Implemented Validation/ user registration/ authentication (jwt)/ middleware to login

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 // registering stuff
 
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -21,14 +24,51 @@ router.post(
       'Please enter a password with 6 or more character'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    return res.status(200).json({ msg: 'user registered succesfully' });
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const paylaod = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        paylaod,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
